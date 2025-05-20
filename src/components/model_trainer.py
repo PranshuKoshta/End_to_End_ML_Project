1a188b2b789853eb75aea576ff983ffb4235ddb8
Pranshu Koshta <pranshukoshta7@gmail.com>
2025-05-20 23:58:26 +0530

    Added Utils function

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -19,7 +19,7 @@ from xgboost import XGBRegressor
 from src.exception import CustomException
 from src.logger import logging
 
-from src.utils import save_object
+from src.utils import save_object, evaluate_models
 
 @dataclass
 class ModelTrainerConfig:
@@ -28,4 +28,41 @@ class ModelTrainerConfig:
 class ModelTrainer:
     def __init__(self):
         self.model_trainer_config = ModelTrainerConfig()
-        self.model = None
+
+    def initiate_model_trainer(self, train_array, test_array,preprocessor_path):
+        try:
+            logging.info("Splitting training and testing input data")
+            X_train, y_train, X_test, y_test = (
+                train_array[:, :-1],
+                train_array[:, -1],
+                test_array[:, :-1],
+                test_array[:, -1],
+            )
+
+            models = {
+                "Logistic Regression": LogisticRegression(),
+                "KNeighbors Classifier": KNeighborsClassifier(),
+                "Decision Tree Classifier": DecisionTreeClassifier(),
+                "Random Forest Classifier": RandomForestClassifier(),
+                "Gradient Boosting Classifier": GradientBoostingClassifier(),
+                "AdaBoost Classifier": AdaBoostClassifier(),
+                "XGBoost Regressor": XGBRegressor(),
+                "CatBoost Classifier": CatBoostClassifier(verbose=0),
+            }
+
+            model_report: dict = evaluate_models(
+                X_train=X_train,
+                y_train=y_train,
+                X_test=X_test,
+                y_test=y_test,
+                models=models,
+            )
+
+            # Save the best model
+            save_object(
+                file_path=self.model_trainer_config.trained_model_file_path,
+                obj=best_model,
+            )
+
+        except Exception as e:
+            raise CustomException(e, sys)
