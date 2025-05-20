c4184ec59644d548a9c398b995a5400f667a3d32
Pranshu Koshta <pranshukoshta7@gmail.com>
2025-05-20 23:47:51 +0530

    minor changes

---
+++ src/components/model_trainer.py
@@ -0,0 +1,31 @@
+import os
+import sys
+
+from dataclasses import dataclass
+
+from catboost import CatBoostClassifier
+from sklearn.ensemble import (
+    AdaBoostClassifier,
+    GradientBoostingClassifier,
+    RandomForestClassifier,
+)
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import r2_score
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from xgboost import XGBRegressor
+
+from src.exception import CustomException
+from src.logger import logging
+
+from src.utils import save_object
+
+@dataclass
+class ModelTrainerConfig:
+    trained_model_file_path = os.path.join("artifacts", "model.pkl")
+
+class ModelTrainer:
+    def __init__(self):
+        self.model_trainer_config = ModelTrainerConfig()
+        self.model = None
